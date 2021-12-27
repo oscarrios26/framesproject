435f6b8c06d888ebfafced20691b0cb8d073feaa
Mayra and Oscar <mayraandoscar@mayras-mbp.attlocal.net>
2021-12-27 11:05:50 -0600

    update

--- products-app/src/components/Homepage/Homepage.jsx
+++ products-app/src/components/Homepage/Homepage.jsx
@@ -4,45 +4,47 @@ import "./Homepage.css";
 import { useState } from "react";
 
 export default function Homepage(props) {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState([]);
+
+
+  const handleSearch = (event) => {
+    const results = props.product.filter((pro) =>
+      pro.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setSearch(results);
+    
+  };
+
+  const handleSubmit = (event) => event.preventDefault();
 
   return (
     <>
         <div id="box" >
-        
+        <form className="search-form" onSubmit={(e) => handleSubmit(e)}>
         <input
           id="searchbarcss"
-          type="search"
+            type="text"
+            name="Search"
           placeholder="search"
           value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
+          onChange={(e) => handleSearch(e)}
           />
-          
+          </form>
       </div>
       <div className="grid">
-        <div className="inner">
-          {props.product.filter((val) => {
-              if (search === "") {
-                return val;
-              } else if (
-                val.name.toLowerCase().includes(search.toLowerCase())) {
-                return val;
-              }
-            })
-            .map((prod, _id) => {
+        
+        {props.product.map((prod, _id) => (
               
-              <div className="hoverEffect" key={_id}>
-                <Link id="textdeco" to={`/details/${prod._id}`}>
-                  <img src={prod.imgURL} alt="pictures" width="350px" />
-                  <h3 className="titlecolor"  >{prod.name}</h3>
-                  <h4 className="pricecolor" >$ {prod.price}</h4>
-                </Link>
-              </div>
-            })}
+          <div className="hoverEffect" key={_id}>
+            <Link id="textdeco" to={`/details/${prod._id}`}>
+              <img src={prod.imgURL} alt="pictures" width="350px" />
+              <h3 className="titlecolor"  >{prod.name}</h3>
+              <h4 className="pricecolor" >$ {prod.price}</h4>
+            </Link>
+          </div>
+        ))}
         </div>
-      </div>
+      
 
 
     
